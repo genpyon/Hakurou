799eca8661e959b54bb8206329133c86cc61c725
genpyon <genpyope@gmail.com>
2020-05-06 02:55:38 +0900

    lib

--- 00_WEREWOLF/src/com/genpyon/Library/lib.java
+++ 00_WEREWOLF/src/com/genpyon/Library/lib.java
@@ -64,6 +64,8 @@ public class lib {
 		return item;
 	}
 
+
+
 	public static ItemStack createEnchantItem(Material material, int am ,Enchantment enc,int Lv, String name, String... list){
 		List<String> lore = Arrays.asList(list);
 		ItemStack item = new ItemStack(material,am);
