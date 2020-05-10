48ca1dbb60d6de3ef014f6ebe1a04a5e6c14f630
gutitubo <gutitubox@gmail.com>
2020-05-10 21:35:15 +0900

    * NullPo修正

--- 00_WEREWOLF/src/com/genpyon/Item/Wolf/Teleport.java
+++ 00_WEREWOLF/src/com/genpyon/Item/Wolf/Teleport.java
@@ -1,6 +1,8 @@
 package com.genpyon.Item.Wolf;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.genpyon.Main;
 import com.genpyon.Item.AbstractItem;
@@ -15,13 +17,21 @@ public class Teleport extends AbstractItem {
 	@Override
 	public ItemStack getItemStack() {
 		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		ItemStack item = new ItemStack(Material.DIAMOND);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName("Teleporter");
+		item.setItemMeta(itemMeta);
+		return item;
 	}
 
 	@Override
 	public ItemStack getIcon() {
 		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		ItemStack item = new ItemStack(Material.DIAMOND);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName("Teleporterですよ");
+		item.setItemMeta(itemMeta);
+		return item;
 	}
 
 }
