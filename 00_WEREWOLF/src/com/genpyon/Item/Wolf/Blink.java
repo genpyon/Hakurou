d95a7d3e947a0feff623a12e9ca71994fef466c6
gutitubo <gutitubox@gmail.com>
2020-05-11 23:20:37 +0900

    Blink途中までつくたよｗ

--- 00_WEREWOLF/src/com/genpyon/Item/Wolf/Blink.java
+++ 00_WEREWOLF/src/com/genpyon/Item/Wolf/Blink.java
@@ -1,5 +1,9 @@
 package com.genpyon.Item.Wolf;
 
+import java.util.HashMap;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.genpyon.Main;
@@ -7,6 +11,8 @@ import com.genpyon.Item.AbstractItem;
 import com.genpyon.ItemStack.GameItemManager;
 
 public class Blink extends AbstractItem {
+
+	public static HashMap<Player, Integer> blinkPower = new HashMap<>();
 
 	public Blink(Main plugin) {
 		super(plugin);
@@ -20,6 +26,27 @@ public class Blink extends AbstractItem {
 	@Override
 	public ItemStack getIcon() {
 		return GameItemManager.BLINK_ICON();
+	}
+
+	public static void chargeBlink(Player p) {
+		Integer power = blinkPower.get(p);
+		if (power == null) {
+			power = new Integer(0);
+			blinkPower.put(p, 0);
+			p.playSound(p.getLocation(), Sound.ENTITY_GHAST_WARN, 0.5F, 1F);
+		}
+
+		blinkPower.put(p, power + 1);
+	}
+
+	public static void releaseBlink(Player p) {
+		Integer power = blinkPower.get(p);
+		if (power == null) {
+			power = new Integer(0);
+			blinkPower.put(p, 0);
+		}
+
+		blinkPower.remove(p);
 	}
 
 }
